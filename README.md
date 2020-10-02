1c430181e67c439a3bd548cb86ff4297de2de91a
bachwilliam17 <bachwilliam17@gmail.com>
2020-10-02 16:58:41 -0400

    création des folders selon le plan de la structure du code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IFT3913-TP1
+
+Rouler JavaAnalyzer.java en passant en argument le chemin absolu du directory contenant les files à inspecter.
--- src/CodeAnalyzer.java
+++ src/CodeAnalyzer.java
@@ -1,6 +1,7 @@
 /***
  * 
- * @author Daniel El-Masri (20096261)
+ * Cette classe calcule les métriques voulues du code Java associé
+ * @author Daniel El-Masri (20096261) && William Bach (20127144)
  *
  */
 public class CodeAnalyzer {
