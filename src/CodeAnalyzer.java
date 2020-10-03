f40dbb9dec8b0de326567cb13b60a96f740d5f2e
bachwilliam17 <bachwilliam17@gmail.com>
2020-10-03 02:38:19 -0400

    FileManager implemented and partially tested

--- src/CodeAnalyzer.java
+++ src/CodeAnalyzer.java
@@ -1,9 +1,126 @@
+import Java.util.io;
+import Java.util.List;
+import java.util.ArrayList;
+
 /***
  * 
- * Cette classe calcule les métriques voulues du code Java associé
+ * Cette classe calcule les metriques voulues du code Java associe
  * @author Daniel El-Masri (20096261) && William Bach (20127144)
  *
  */
-public class CodeAnalyzer {
+public class CodeAnalyzer 
+{
+    String path; //chemin absolu du folder contenant les files a analyser
+    String[][] classes; //2d array avec chaque ligne de chaque classe
+    String[] classNames; //noms des classes associees 
+    List<List<int[]>> methods; //index des méthodes de chaque classe
+    List<List<int[]>> methodNames; //noms des methodes associees
+
+    /**
+     * Constructeur. Enregistre les attributs
+     * @param path: chemin absolu du folder avec les files
+     */
+    public CodeAnalyzer(String path) 
+    {
+        this.path = path;
+    }
+
+    /**
+     * Recupere les lignes de chaque classe grace a FileManager
+     * @param path
+     * @return 2d array avec les lignes de chaque classe
+     */
+    public String[][] getClasses(String path) 
+    {
+        //TODO
+    }
+
+    /**
+     * Renvoie ls index (= no de 'ligne' dans classes) des methodes de chq classe.
+     * On utilise une ArrayList car on ne sait pas a l'avance combien de methodes
+     * une classe va avoir.
+     * @return index des methodes de chaque classe
+     */
+    public List<List<int[][]>> getMethodes()
+    {
+        //TODO
+    }
+
+    /**
+     * Produit les files CSV pour sauvegarder les .
+     */
+    public void produceCSV()
+    {
+        //TODO
+    }
+
+
+    /**
+     * Calcule le nombre de lignes de code non-vides dans une classe
+     * @param allLines les lignes de la classe
+     * @return 
+     */
+    private int classe_LOC(ArrayList<String> allLines)
+    {
+        //TODO
+    }
+
+    /**
+     * Calcule le nombre de lignes de codes non-vides dans une methode
+     * @param allLines les lignes de la methode
+     * @return
+     */
+    private int methode_LOC(ArrayList<String> allLines)
+    {
+        //TODO
+    }
+
+    /**
+     * Calcule le nombre de lignes-commentaires dans une classe
+     * @param allLines
+     * @return
+     */
+    private int classe_CLOC(ArrayList<String> allLines)
+    {
+        //TODO
+    }
+
+    /**
+     * Calcule le nombre de lignes-commentaires dans une methode
+     * @param allLines
+     * @return
+     */
+    private int methode_CLOC(ArrayList<String> allLines)
+    {
+        //TODO
+    }
+
+    /**
+     * Calcule la densite de commentaires d'une classe
+     * @param alLines
+     * @return
+     */
+    private float classe_DC(ArrayList<String> alLines)
+    {
+        //TODO
+    }
+
+    /**
+     * Calcule la densite de commentaires dans une methode
+     * @param alLines
+     * @return float car valeur non-entiere (pourcentage)
+     */
+    private float methode_DC(ArrayList<String> alLines)
+    {
+        //TODO
+    }
+
+    public static void main(String[] args) 
+    {
+        String folderToAnalyze = args[0];
+        CodeAnalyzer analyzer = new CodeAnalyzer(folderToAnalyze);
+
+        //TODO
+    }
 
 }
--- src/util/FileManager.java
+++ src/util/FileManager.java
@@ -1,8 +1,136 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
 /***
- * 
- * @author daniel
+ * FileManager handles the search and extraction of all .java files in
+ * the folder to analyze.
+ * @author William Bach (20127144)
  *
  */
 public class FileManager {
+    String path; //absolute path of the folder to analyze
+
+    /**
+     * Constructor
+     * @param path attribute 
+     */
+    public FileManager(String path)
+    {
+        this.path = path;
+    }
+
+    /**
+     * Sends to CodeAnalyzer every line of every .java file in the attribute 
+     * folder
+     * @return 2D Strings array
+     * 
+     * SOURCE: https://stackabuse.com/java-list-files-in-a-directory/
+     */
+    public String[][] getClassesArray()
+    {
+        //get the .java File objects 
+        File[] classes = listFiles(this.path);
+
+        //store the lines of every file found
+        ArrayList<ArrayList<String>> classesArray = new ArrayList<ArrayList<String>>();
+        //extract every line of every File
+        for (var i=0; i<classes.length; i++) 
+        {
+            try {
+                List<String> allLines = Files.readAllLines(
+                    Paths.get(classes[i].getAbsolutePath()));
+                    System.out.println(allLines);
+                //convert List of lines to ArrayList for convenience
+                ArrayList<String> allLinesArrayList = new ArrayList<String>(allLines);
+                //add to the @return array
+                classesArray.add(allLinesArrayList);
+            } 
+            catch (IOException e) {
+                System.out.println("Something went wrong (getClassesArray)");
+                System.exit(1);
+            }
+        }
+
+        //convert ArrayList to a String array for the main class
+        String[][] finalArray = new String[classesArray.size()][];
+        for (var i=0; i<classesArray.size(); i++)
+        {
+            ArrayList<String> fileContent = classesArray.get(i);
+            finalArray[i] = fileContent.toArray(new String[fileContent.size()]);
+        }
+        return finalArray;
+
+    }
+
+    /**
+     * Sends to CodeAnalyzer the name of all the classes to be analyzed
+     * @return String array of names
+     */
+    public String[] getClassNamesArray() 
+    {
+        File[] classes = listFiles(this.path);
+        String[] classNames = new String[classes.length];
+
+        for (var i=0; i<classes.length; i++)
+        {
+            classNames[i] = classes[i].getName();
+            System.out.println(classNames[i]);
+        } 
+
+        return classNames;
+        
+    }
+
+    /**
+     * Searches the folder  
+     * @param startDir folder
+     * 
+     * SOURCE: https://stackabuse.com/java-list-files-in-a-directory/
+     */
+    private File[] listFiles(String startDir) 
+    {
+        File[] files = null;
+
+        try {
+            //extracts files from up to 20 inner folders
+            files = Files.walk(Paths.get(startDir), 20)
+            .filter(Files::isRegularFile)
+            .map(Path::toFile)
+            .toArray(File[]::new);
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+            System.exit(1);
+        }
+    
+        //extraction of files .java 
+        File[] javaFiles = new File[files.length];
+        int counter = 0;
+        for (var i=0; i<files.length; i++)
+        {
+            if (files[i].getName().endsWith(".txt")) {
+                javaFiles[counter] = files[i];
+                counter++;
+            }
+        }
+        return javaFiles;
+        
+    }
+    
+    public static void main(String[] args) 
+    {
+        System.out.println(args[0]);
+        FileManager test = new FileManager(args[0]);
+        test.getClassNamesArray();
+    }
+    
+
+
 
 }
+    
