cbb93b34ac2067ac313cb9c028700ea18877edc2
Daniel El-Masri <daniel.elmasri@gmail.com>
2020-10-07 11:34:32 -0400

    Added CSVManager Test Class

---
+++ src/CSVManagerTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class CSVManagerTest {
+
+	/***
+	 * Private Fields
+	 */
+	private CSVManager manager;
+	
+	/***
+	 * Initiation methods pre-testing executions
+	 */
+	@Before
+	public void initTest()
+	{
+		manager = new CSVManager();
+	}
+	
+	// Test Methods
+	
+	/***
+	 * Test Method 1 : 
+	 */
+	@Test
+	void updateCSVFile_ShouldBeSuccessful() {
+		fail("Not yet implemented");
+	}
+	
+	/***
+	 * Test Method 2 : 
+	 */
+	@Test
+	void updateCSVFile_ShouldNotBeSuccessful() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
